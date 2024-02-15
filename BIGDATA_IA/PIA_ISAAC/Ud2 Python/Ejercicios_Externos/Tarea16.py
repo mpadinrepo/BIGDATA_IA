986f213254fbd645ce405e6ba9576383564a576e
mpadinrepo <manuelpadinpresa@gmx.es>
2024-02-15 22:12:44 +0100

    OK Segunda tanda 20240215

--- BIGDATA_IA/PIA_ISAAC/Ud2 Python/Ejercicios_Externos/Tarea16.py
+++ BIGDATA_IA/PIA_ISAAC/Ud2 Python/Ejercicios_Externos/Tarea16.py
@@ -1,7 +1,7 @@
 # se ingresa por teclado un valor entero,mostrar una leyenda que indique
 #si el numer es positivo,negativo o nulo (es decir ZERO)
 
-numero = int(input("Ingrese por teclado un valor entero: "))
+numero = int(input=("Ingrese por teclado un valor entero: "))
 
 if numero == 0:
     print ("el valor introducido es nulo")
