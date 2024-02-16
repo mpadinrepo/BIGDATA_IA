95a5ce86b058bbe32dc59dcf69ed1eebaf0b4c75
Manuel Padin <manuelpadinpresa@gmx.es>
2024-02-16 09:05:04 +0100

    OK GO

--- BIGDATA_IA/BDA_RICARDO/Módulo BDA 2023-2024/UD1/Prácticas/03 Práctica de execución de traballos mapreduce Python en Hadoop/Hadoop_CONF/Choiva/mapperMaxChoivav3.py
+++ BIGDATA_IA/BDA_RICARDO/Módulo BDA 2023-2024/UD1/Prácticas/03 Práctica de execución de traballos mapreduce Python en Hadoop/Hadoop_CONF/Choiva/mapperMaxChoivav3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: iso-8859-15 -*-
-
 '''
 O formato de cada liña do ficheiro de entrada é:
 "Código validación","Data","Código parámetro","Parámetro","Valor","Unidades"
@@ -9,16 +8,15 @@ para cada lectura e devolvelo precedido de só o número de ano
 Para cada lectura obtemo-los pares <ano, choiva_recollida>
 p.e.: 2023 4.2
 '''
-
 import sys
-
 # Iterar sobre as liñas de entrada dende sys.stdin
 for linha in sys.stdin:
-    # Eliminar espazos en branco ó principio e ó final da liña
-    linha = linha.strip()
-    # Descompo-la liña en campos separados por comas
-    codigo, instante_lectura, lixo1, lixo2, choiva, lixo3 = linha.split(",")
-    # Verificar se o string de código (eliminando as comiñas) está na lista ["1", "5"]
-    if codigo.strip('"') in ["1", "5"]:
-        # Imprimi-lo ano e a cantidade de choiva
-        print(instante_lectura[1:5], choiva.strip('"'))
+# Eliminar espazos en branco ó principio e ó final da liña
+linha = linha.strip()
+# Descompo-la liña en campos separados por comas
+codigo, instante_lectura, lixo1, lixo2, choiva, lixo3 = linha.split(",")
+# Verificar se o string de código (eliminando as comiñas) está na lista ["1",
+"5"]
+if codigo.strip('"') in ["1", "5"]:
+# Imprimi-lo ano e a cantidade de choiva
+print(instante_lectura[1:5],choiva.strip('"'))
