fe8de700f4773cc219ca4cbeea76eadaf8b5d832
mpadinrepo <manuelpadinpresa@gmx.es>
2024-03-26 03:06:03 +0100

    20240326 Prolog SWI JupyterLab OK _ ML

--- BIGDATA_IA/MIA_DAVID/PROLOG/venv/Lib/site-packages/prolog_kernel/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Jupyter kernel for Prolog"""
-
-from .kernel import PrologKernel
-
-__version__ = PrologKernel.implementation_version
--- BIGDATA_IA/MIA_DAVID/PROLOG/venv/Lib/site-packages/prolog_kernel/__main__.py
+++
@@ -1,4 +0,0 @@
-from ipykernel.kernelapp import IPKernelApp
-from prolog_kernel.kernel import PrologKernel
-
-IPKernelApp.launch_instance(kernel_class=PrologKernel)
